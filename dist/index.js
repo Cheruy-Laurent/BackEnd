9de48f7fcd337b4f8a0284804428ec2828b1b394
Cheruy-Laurent <laurentcheruy@free.fr>
2021-06-29 15:24:48 +0200

    Crud

--- dist/index.js
+++ dist/index.js
@@ -7,29 +7,34 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const book_model_1 = __importDefault(require("./book.model"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const serve_static_1 = __importDefault(require("serve-static"));
 const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
 app.use(body_parser_1.default.json());
-app.use(serve_static_1.default("public"));
 app.use(cors_1.default());
-const uri = "mongodb://localhost:27017/BIBLI02";
+const uri = "mongodb://localhost:27017/BIBLIO2";
 mongoose_1.default.connect(uri, (err) => {
     if (err)
         console.log(err);
     else
-        console.log("Mongo Data");
+        console.log("Mongo Data base connected successfuly");
 });
 app.get("/", (req, resp) => {
-    resp.send("heelo");
+    resp.send("Hello Express");
 });
 app.get("/books", (req, resp) => {
-    // resp.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     book_model_1.default.find((err, books) => {
         if (err)
             resp.status(500).send(err);
         else
             resp.send(books);
+    });
+});
+app.get("/books/:_id", (req, resp) => {
+    book_model_1.default.findById(req.params._id, (err, book) => {
+        if (err)
+            resp.status(500).send(err);
+        else
+            resp.send(book);
     });
 });
 app.post("/books", (req, resp) => {
@@ -42,11 +47,11 @@ app.post("/books", (req, resp) => {
     });
 });
 app.put("/books/:_id", (req, resp) => {
-    book_model_1.default.findByIdAndUpdate(req.params._id, req.body, (err, book) => {
+    book_model_1.default.findByIdAndUpdate(req.params._id, req.body, (err) => {
         if (err)
             resp.status(500).send(err);
         else
-            resp.send("Book updated successfull");
+            resp.send("Book updated succeslully");
     });
 });
 app.delete("/books/:_id", (req, resp) => {
@@ -57,27 +62,29 @@ app.delete("/books/:_id", (req, resp) => {
             resp.send("Book delete successfull");
     });
 });
+/*GET http://localhost:8085/pbooks?page=1&size=5*/
 app.get("/pbooks", (req, resp) => {
     let p = parseInt(req.query.page || 1);
-    let size = parseInt(req.query.page || 5);
-    book_model_1.default.paginate({}, { page: p, limit: size }, function (err, result) {
+    let size = parseInt(req.query.size || 5);
+    book_model_1.default.paginate({}, { page: p, limit: size }, (err, books) => {
         if (err)
             resp.status(500).send(err);
         else
-            resp.send(result);
+            resp.send(books);
     });
 });
+/*GET http://localhost:8085/books-search?kw=page=1&size=5*/
 app.get("/books-search", (req, resp) => {
     let p = parseInt(req.query.page || 1);
-    let size = parseInt(req.query.page || 5);
+    let size = parseInt(req.query.size || 5);
     let kw = req.query.kw || "";
-    book_model_1.default.paginate({ title: { $regex: ".*(?i)" + kw + ".*" } }, { page: p, limit: size }, function (err, result) {
+    book_model_1.default.paginate({ title: { $regex: ".*(?i)" + kw + ".*" } }, { page: p, limit: size }, (err, books) => {
         if (err)
             resp.status(500).send(err);
         else
-            resp.send(result);
+            resp.send(books);
     });
 });
 app.listen(8085, () => {
-    console.log("server");
+    console.log("Serve started");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,51 @@
-import express, {query, Request, Response} from "express";
+import express, {Request, Response} from "express";
 import mongoose from "mongoose";
 import Book from "./book.model";
 import bodyParser from "body-parser";
-import serveStatic from "serve-static";
 import cors from "cors";
 
 const app=express();
 app.use(bodyParser.json());
-app.use(serveStatic("public"));
 app.use(cors());
-const uri = "mongodb://localhost:27017/BIBLI02";
-mongoose.connect(uri, (err) => {
-    if (err) console.log(err);
-   else     console.log("Mongo Data");
+
+const uri="mongodb://localhost:27017/BIBLIO2";
+mongoose.connect(uri,(err)=>{
+    if(err) console.log(err);
+    else console.log("Mongo Data base connected successfuly");
 });
 
-app.get("/", (req :Request, resp: Response) => {
-    resp.send("heelo");
+app.get("/",(req:Request,resp:Response)=>{
+    resp.send("Hello Express");
 });
 
-
-app.get("/books", (req :Request, resp: Response) => {
-   // resp.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    Book.find((err, books) => {
-        if (err)
-            resp.status(500).send(err);
-        else
-       
-            resp.send(books);
+app.get("/books",(req:Request,resp:Response)=>{
+   Book.find((err,books)=>{
+       if(err) resp.status(500).send(err);
+       else resp.send(books);
     });
 });
 
-
-app.post("/books", (req :Request, resp: Response) => {
-    let book =new Book(req.body);
-    book.save(err=>{
-        if (err)
-        resp.status(500).send(err);
-        else
-            resp.send(book);
-        });
+app.get("/books/:_id",(req:Request,resp:Response)=>{
+    Book.findById(req.params._id,(err,book)=>{
+        if(err) resp.status(500).send(err);
+        else resp.send(book);
+    });
 });
 
-app.put("/books/:_id", (req :Request, resp: Response) => {
+app.post("/books",(req:Request,resp:Response)=>{
+    let book =new Book(req.body);
+    book.save(err=>{
+        if(err) resp.status(500).send(err);
+        else resp.send(book);
+    })
 
-    Book.findByIdAndUpdate(req.params._id,req.body,(err,book)=>{
-        if (err)
-        resp.status(500).send(err);
-        else
-            resp.send("Book updated successfull");
-        });
+});
+app.put("/books/:_id",(req:Request,resp:Response)=>{
+    Book.findByIdAndUpdate(req.params._id,req.body,(err)=>{
+        if(err) resp.status(500).send(err);
+        else resp.send("Book updated succeslully");
+    })
+
 });
 
 app.delete("/books/:_id", (req :Request, resp: Response) => {
@@ -62,32 +58,29 @@ app.delete("/books/:_id", (req :Request, resp: Response) => {
         });
 });
 
-app.get("/pbooks", (req:Request, resp: Response) => {
 
-let p :number = parseInt(req.query.page as any||1);
-let size :number = parseInt(req.query.page as any||5);
 
-    Book.paginate({},{page : p , limit: size }, function (err, result) {
-        if (err)
-            resp.status(500).send(err);
-        else
-            resp.send(result);
+/*GET http://localhost:8085/pbooks?page=1&size=5*/
+app.get("/pbooks",(req:Request,resp:Response)=>{
+    let p:number=parseInt(req.query.page as any|| 1);
+    let size:number=parseInt(req.query.size as any|| 5);
+    Book.paginate({},{page:p, limit:size},(err,books)=>{
+        if(err) resp.status(500).send(err);
+        else resp.send(books);
     });
 });
 
-app.get("/books-search", (req:Request, resp: Response) => {
-
-    let p :number = parseInt(req.query.page as any||1);
-    let size :number = parseInt(req.query.page as any||5);
-    let kw :string = req.query.kw as any || "";
-    
-        Book.paginate({title:{$regex:".*(?i)"+kw+".*"}},{page : p , limit: size }, function (err, result) {
-            if (err)
-                resp.status(500).send(err);
-            else
-                resp.send(result);
-        });
+/*GET http://localhost:8085/books-search?kw=page=1&size=5*/
+app.get("/books-search",(req:Request,resp:Response)=>{
+    let p:number=parseInt(req.query.page as any|| 1);
+    let size:number=parseInt(req.query.size as any || 5);
+    let kw:string=req.query.kw as any || "";
+    Book.paginate({title:{$regex:".*(?i)"+kw+".*"}},{page:p, limit:size},(err,books)=>{
+        if(err) resp.status(500).send(err);
+        else resp.send(books);
     });
-app.listen(8085, () => {
-    console.log("server");
 });
+
+app.listen(8085,()=>{
+    console.log("Serve started");
+})
